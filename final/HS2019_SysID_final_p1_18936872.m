c7ad03a155e91ba54b98eefd8c2218f67e9cbfb1
ossamaAhmed <ossama.ahmed@mail.mcgill.ca>
2019-12-12 01:44:32 +0100

    added the problem 1 part 2

--- final/HS2019_SysID_final_p1_18936872.m
+++ final/HS2019_SysID_final_p1_18936872.m
@@ -185,8 +185,8 @@ end
 R_inverse_Phi = R \ Phi;
 Z = R_inverse_Phi / (Phi.' * R_inverse_Phi);
 theta_estimated = Z.' * Y;
-p1_a_estimated = theta_estimated(1:3);
-p1_b_estimated = theta_estimated(4:6);
+p1_a_estimated = theta_estimated(1:3)
+p1_b_estimated = theta_estimated(4:6)
 %-----------------------------------------------------------------------
 %Self Validation Now
 %-----------------------------------------------------------------------
@@ -237,7 +237,7 @@ axes.YLabel.FontSize = 14;
 
 estimation_error_var = var(estimation_error(4:end), 1);
 fprintf('Estimated Model ouput error variance is: %.4f.\n', estimation_error_var);
-fprintf('Estimated Model ouput error is: %.4f.\n', sum(estimation_error(:)));
+fprintf('Estimated Model ouput error is: %.4f.\n', sum(abs(estimation_error(4:end))));
 
 
 p1_theta_est1 = theta_estimated;
@@ -246,57 +246,81 @@ p1_Phi = Phi;
 %% Task 2: Improve your estimate
 c1 = 1;
 c2 = pi/4;
+z = tf('z');
 C_z = 1 + c1 / z + c2 / (z^2);
-C_z_inverse = 1 / C_z;
-C_z_poles = pole(C_z_inverse);
-fprintf('C(z)^(-1) might be stable with only a pole at 0 which might not be stable\n');
-%now lets try to see if the C_inv is stable
-u_f = lsim(C_z_inverse, p1_u);
-y_f = lsim(C_z_inverse, p1_y);
-figure(figure_num);
-figure_num = figure_num + 1;
-fig = gcf;
-fig.Position = [mod(figure_num,2)*screenwidth/2, 0, screenwidth/2, screenheight];
-plot(u_f);
-hold on;
-plot(y_f);
-axis tight;
-axes = gca;
-axes.Title.Interpreter = 'latex';
-axes.Title.String = 'Pre-filtered Input vs. Output';
-axes.Title.FontSize = 18;
-axes.XAxis.TickLabelInterpreter = 'latex';
-axes.XAxis.FontSize = 10;
-axes.YAxis.TickLabelInterpreter = 'latex';
-axes.YAxis.FontSize = 10;
-axes.XLabel.Interpreter = 'latex';
-axes.XLabel.String = '$k$ $[samples]$';
-axes.XLabel.FontSize = 14;
-lgd = legend('$u_{F}(k)$', '$y_{F}(k)$');
-lgd.Interpreter = 'latex';
-lgd.FontSize = 12;
-lgd.Location = 'northwest';
-p1_theta_est2 = [];
-%the filtered input and output seems to not blow up
-%lets get our estimate
+
+fprintf('A linear model is derived for BLUE estimate: Y = Phi*Theta + Epsilon\n');
+fprintf('where, \n');
+fprintf('Y(k) = y(k)\n');
+fprintf('Theta = [a1; a2; a3; b1; b2; b3]\n');
+fprintf('Phi(k) = [-y(k-1) -y(k-2) -y(k-3) u(k-1) u(k-2) u(k-3)]\n');
+fprintf('Epsilon(k) = e(k)\n');
+fprintf('Error covariance matrix R is expected value of (Epsilon * Epsilon^T) and is taken from the question formulation:\n');
+fprintf('EXPLAIN HERE\n');
+
+%-----------------------------------------------------------------------
+%Getting the Covariance Matrix
+%-----------------------------------------------------------------------
+
+%forming covariance matrix R now
+N2 = N - 5;
+R = zeros(N2);
+for i = 1 : N2
+    for j = 1 : N2
+        if i == j
+%             R(i,j) = 0.2*(c1 + c2 + c1 + c1*c2 + c2 + c1*c2) + 0.9*(1 + c2^2 + c1^2);
+            R(i,j) = 0.9*(1 + c2^2 + c1^2) + 0.2*(c1 + c2 + c1 + c1*c2 + c2 + c1*c2);
+        elseif abs(i - j) == 1
+%             R(i,j) = 0.2*(1 + c1 + c2 + c2^2 + c1^2 + c1*c2 + c2) + 0.9*(c1 + c1*c2);
+            R(i,j) = 0.2*(1 + c1 + c2 + c2^2 + c1^2 + c1*c2 + c2) + 0.9*(c1 + c1*c2);
+        elseif abs(i - j) == 2
+%             R(i,j) = 0.2*(1 + c1 + c1 + c1^2 + c1*c2 + c1*c2 + c2^2) + c2*0.9;
+            R(i,j) = 0.2*(1 + c1 + c1 + c1^2 + c1*c2 + c1*c2 + c2^2) + c2*0.9;
+        elseif abs(i - j) == 3
+            R(i,j) = (1 + c1 + c1^2 + c2 + c1*c2 + c2^2) * 0.2;
+        elseif abs(i - j) == 4
+            R(i,j) = (c1 + c2 + c1*c2) * 0.2;
+        elseif abs(i - j) == 5
+            R(i,j) = c2 * 0.2;
+        end
+    end
+end
+
+%now lets confirm if R is symmetric
+R_difference = abs(R - R.');
+R_difference_max = max(R_difference(:));
+R_difference_min = min(R_difference(:));
+fprintf('It is confirmed that R is symmetric for checking R - R^T maximum: %.4f and minimum: %.4f\n', R_difference_max, R_difference_min);
+min_eigen_value_R = min(eig(R));
+fprintf('It is confirmed that R is positive definite for checking the smallest eigen value of R > 0: %.4f\n', min_eigen_value_R);
+
 %-----------------------------------------------------------------------
 %Getting the Phi Matrix
 %-----------------------------------------------------------------------
-phi_f = zeros(N, n_parameters);
-phi_f(2,:) = [-y_f(1) 0 0 u_f(1) 0 0];
-phi_f(3,:) = [-y_f(2) -y_f(1) 0 u_f(2) u_f(1) 0];
-for k = 4 : N
-    phi_f(k,:) = [-y_f(k-1) -y_f(k-2)  -y_f(k-3) u_f(k-1) u_f(k-2) u_f(k-3)];
+%now lets compute the BLUE estimator from slide 9.32
+
+%start by forming phi now
+n_parameters = 6;
+%cutting off the first 3 elements
+Phi = zeros(N2, n_parameters);
+Y = p1_y(6:end);
+for k = 6 : N
+    Phi(k-5,:) = [-p1_y(k-1) -p1_y(k-2) -p1_y(k-3) p1_u(k-1) p1_u(k-2) p1_u(k-3)];
 end
+
 %-----------------------------------------------------------------------
 %Getting the theta estimate now
 %-----------------------------------------------------------------------
-theta_estimated_f = phi_f \ y_f;
+R_inverse_Phi = R \ Phi;
+Z = R_inverse_Phi / (Phi.' * R_inverse_Phi);
+theta_estimated = Z.' * Y;
+p1_a_estimated = theta_estimated(1:3)
+p1_b_estimated = theta_estimated(4:6)
 %-----------------------------------------------------------------------
 %Self Validation Now
 %-----------------------------------------------------------------------
-y_f_estimated = phi_f * theta_estimated_f;
-y_estimated = lsim(C_z, y_f_estimated);
+y_estimated = zeros(N,1);
+y_estimated(6:end) = Phi * theta_estimated;
 estimation_error = p1_y - y_estimated;
 figure(figure_num);
 figure_num = figure_num + 1;
@@ -340,10 +364,41 @@ axes.YLabel.Interpreter = 'latex';
 axes.YLabel.String = '$y(k) - y_{\hat{\theta}}(k)$';
 axes.YLabel.FontSize = 14;
 
-estimation_error_var = var(estimation_error(:), 1);
+estimation_error_var = var(estimation_error(6:end), 1);
 fprintf('Improved Estimated Model ouput error variance is: %.4f.\n', estimation_error_var);
-fprintf('Improved Estimated Model ouput error is: %.4f.\n', sum(estimation_error(:)));
+fprintf('Improved Estimated Model ouput error is: %.4f.\n', sum(abs(estimation_error(6:end))));
 
+
+
+%-----------------------------------------------------------------------
+%Getting the theta estimate now
+%-----------------------------------------------------------------------
+c1 = 1;
+c2 = pi/4;
+C_z = 1 + c1 / z + c2 / (z^2);
+C_z_inverse = 1 / C_z;
+C_z_poles = pole(C_z_inverse);
+fprintf('C(z)^(-1) might be stable with only a pole at 0 which might not be stable\n');
+%now lets try to see if the C_inv is stable
+u_f = lsim(C_z_inverse, p1_u);
+y_f = lsim(C_z_inverse, p1_y);
+
+%-----------------------------------------------------------------------
+%Self Validation Now
+%-----------------------------------------------------------------------
+phi_f = zeros(N, n_parameters);
+phi_f(2,:) = [-y_f(1) 0 0 u_f(1) 0 0];
+phi_f(3,:) = [-y_f(2) -y_f(1) 0 u_f(2) u_f(1) 0];
+for k = 4 : N
+    phi_f(k,:) = [-y_f(k-1) -y_f(k-2)  -y_f(k-3) u_f(k-1) u_f(k-2) u_f(k-3)];
+end
+theta_estimated_f = phi_f \ y_f;
+y_f_estimated = phi_f * theta_estimated_f;
+y_estimated = lsim(C_z, y_f_estimated);
+estimation_error = p1_y - y_estimated;
+estimation_error_var = var(estimation_error(6:end), 1);
+fprintf('Improved Estimated Model ouput error variance is: %.4f.\n', estimation_error_var);
+fprintf('Improved Estimated Model ouput error is: %.4f.\n', sum(abs(estimation_error(6:end))));
 
 %% Task 3: Compute prediction
 
